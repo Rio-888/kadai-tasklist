fcd4a580ae73fc9d4dc0672ca74f5a9fd4e10434
rio.ikegame <0gg326281rg395a@ezweb.ne.jp>
2020-01-23 14:57:24 +0000

    fix lavel of table and add validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_01_23_132703) do
+ActiveRecord::Schema.define(version: 2020_01_23_143808) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "content"
+    t.string "tasks"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
